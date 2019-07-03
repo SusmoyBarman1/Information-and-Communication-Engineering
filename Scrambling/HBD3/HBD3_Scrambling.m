686391d47f82fd53aa084a2cdc4b7475feed5ef7
Susmoy Barman <susmoy.barman.bd@gmail.com>
2019-07-03 10:42:02 +0600

    Added some upgrade version

--- Scrambling/HBD3/HBD3_Scrambling.m
+++ Scrambling/HBD3/HBD3_Scrambling.m
@@ -3,17 +3,6 @@ close all
 clc
 
 x=[1 1 0 1 0 0 0 0 1 1 0 0 0 0 0 0 1 0 0];
- 
- 
-for i=1:1:length(x)
-    
-    t=i:0.001:(i+1);
-    
-    subplot (2,1,1),plot(t,x(i),'r*'),xlabel('temps'),ylabel('valeurs des bits'),title('X(t)')
-    hold on
-    grid on
-    
-end
 
 y=Codage_HDB3(x);
 x
@@ -29,3 +18,38 @@ for i=1:1:length(y)
 end 
 figure,stairs(x)
 figure,stairs(y)
+
+% Decoding
+while (i<(length(y)+1) && i+3<=length(y))
+    if(y(i)==0 && y(i+1)==0 && y(i+2)==0 && y(i+3)>0)
+        z(i)=0;
+        z(i+1)=0;
+        z(i+2)=0;
+        z(i+3)=0;
+        i = i + 4;
+    elseif (y(i)==0 && y(i+1)==0 && y(i+2)==0 && y(i+3)<0)
+        z(i)=0;
+        z(i+1)=0;
+        z(i+2)=0;
+        z(i+3)=0;
+        i = i + 4;
+    elseif (y(i)<0 && y(i+1)==0 && y(i+2)==0 && y(i+3)<0)
+        z(i)=0;
+        z(i+1)=0;
+        z(i+2)=0;
+        z(i+3)=0;
+        i = i + 4;
+    elseif (y(i)>0 && y(i+1)==0 && y(i+2)==0 && y(i+3)>0)
+        z(i)=0;
+        z(i+1)=0;
+        z(i+2)=0;
+        z(i+3)=0;
+        i = i + 4;
+    else
+        if(y(i)>0 || y(i)<0)
+            z(i) = 1;
+            i = i+1;
+        end
+    end
+end
+z
